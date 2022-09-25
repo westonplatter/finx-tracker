42e33e066ae7648342a62f567e78bd7f17dbd903
westonplatter <westonplatter@gmail.com>
2022-09-25 15:09:43 -0600

    create dump and restore django scripts

---
+++ scripts/db_import.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+def run():
+    pg_restore_command = f"PGPASSWORD={os.environ['POSTGRES_PASSWORD']} pg_restore"
+    args  = f" -h {os.environ['POSTGRES_HOST']}"
+    args += f" -U {os.environ['POSTGRES_USER']}"
+    args += f" -d {os.environ['POSTGRES_DB']}"
+    args +=  " --no-privileges"
+    args +=  " --no-owner"
+    args +=  " data/finx-tracker.dump"
+    command = f"{pg_restore_command} {args}"
+    subprocess.run(command, shell=True)
